6bf775e61890890f2d716bc401717b0930f838f0
lisp719 <368034+lisp719@users.noreply.github.com>
2023-02-21 19:33:20 +0900

    feat: Add diesle example

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use self::models::{NewPost, Post};
+use diesel::{Connection, PgConnection, RunQueryDsl};
+use std::env;
+
+pub mod models;
+pub mod schema;
+
+pub fn establish_connection() -> PgConnection {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut PgConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
